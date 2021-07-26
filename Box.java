c19d7eacbcec913737962794d00566a151522ff1
Prabhuchandmakkapati <87955098+Prabhuchandmakkapati@users.noreply.github.com>
2021-07-26 08:13:03 +0530

    Add files via upload

---
+++ Box.java
@@ -0,0 +1,21 @@
+public class Box {
+int size = 0;
+
+public Box() {} // method with no parameters and void return type
+
+public static void main(String[] args) {
+		Box objBox = new Box();  //Box object created
+	        objBox.size = 5;
+		objBox.printBox();			
+		}
+		public void printBox() {
+			final int MAX_ROWS = 5;
+			 for (int row = 1; row <= MAX_ROWS; row++)
+			 {
+			 for (int star = 1; star <= row; star++)
+			 System.out.print("*");
+			 System.out.println();
+	                 }
+		
+	        }
+         }
